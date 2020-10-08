981f005cffb87eef8ee77695a972d09b68c61d80
Shin <huutrimega@gmail.com>
2020-10-08 16:47:04 +0700

    Updates

---
+++ precache-manifest.b3ed2009d3eb8c4c39413e516650e29e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35304554bf768e5cb27a024803873725",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "f1d16e6e31fad86ca72f",
+    "url": "/todo-app/static/css/main.1379bfac.chunk.css"
+  },
+  {
+    "revision": "c24ba9312f8723ff5c50",
+    "url": "/todo-app/static/js/2.88fc7604.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo-app/static/js/2.88fc7604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1d16e6e31fad86ca72f",
+    "url": "/todo-app/static/js/main.5c3f19a5.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
